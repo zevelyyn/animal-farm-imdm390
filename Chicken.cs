b7c661b65b4bc439d36ce0de83d0b850bd4e0150
katherine <111337949+kath3rine@users.noreply.github.com>
2024-04-11 22:36:42 -0400

    Add files via upload

--- Chicken.cs
+++ Chicken.cs
@@ -2,27 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Chicken : Monobehavior {
+public class Chicken : MonoBehaviour {
     // pathways to prefabs and sounds (can be changed)
-    const public GameObject[4] prefabs = [Resources.Load<GameObject>("Assets/Resources/healthyChicken.png"),
-                                         Resources.Load<GameObject>("Assets/Resources/fatChicken.png"), 
-                                         Resources.Load<GameObject>("Assets/Resources/steroidChicken.png"), 
-                                         Resources.Load<GameObject>("Assets/Resources/sickChicken.png")];
-    const public AudioClip[4] sounds = [Resources.Load<AudioClip>("Assets/Resources/healthyAudio"),
-                                        Resources.Load<AudioClip>("Assets/Resources/fatAudio"),
-                                        Resources.Load<AudioClip>("Assets/Resources/steroidAudio"), 
-                                        Resources.Load<AudioClip>("Assets/Resources/sickAudio")];
+    public GameObject[] prefabs = new GameObject [] 
+        {Resources.Load<GameObject>("Assets/Resources/healthyChicken.png"),
+        Resources.Load<GameObject>("Assets/Resources/fatChicken.png"), 
+        Resources.Load<GameObject>("Assets/Resources/steroidChicken.png"), 
+        Resources.Load<GameObject>("Assets/Resources/sickChicken.png")};
+    public AudioClip[] sounds = new AudioClip[] 
+        {Resources.Load<AudioClip>("Assets/Resources/healthyAudio"),
+        Resources.Load<AudioClip>("Assets/Resources/fatAudio"),
+        Resources.Load<AudioClip>("Assets/Resources/steroidAudio"), 
+        Resources.Load<AudioClip>("Assets/Resources/sickAudio")};
     public GameObject MyPrefab;
     public AudioClip MySound;
     void Start() {
-        MyChicken(prefabs[0], audios[0], new Vector3(0, 0, 0), Quaternion.identity); // instantiate healthy chicken
+        MyChicken(prefabs[0], sounds[0], new Vector3(0, 0, 0), Quaternion.identity); // instantiate healthy chicken
     }
 
     void Update() {
     }
 
 
-    public void MyChicken(GameObject prefab, Audioclip sound, Vector3 pos, Quaternion rot) {
+    public void MyChicken(GameObject prefab, AudioClip sound, Vector3 pos, Quaternion rot) {
         GameObject chickenObj = Instantiate(prefab, pos, rot);
         Chicken chicken = chickenObj.AddComponent<Chicken>();
         AudioSource audioSource = chickenObj.AddComponent<AudioSource>();
@@ -40,8 +42,8 @@ public class Chicken : Monobehavior {
     }
 
     // create new chicken w/ same status
-    public void NewChicken(Vector3 posOffset, Quaternion rotOffset) { // new chicken's position and rotation WRT curr chicken, respectively
-        MyChicken(this.MyPrefab, this.MySound, transform.position + posOffset, transform.rotation + rotOffset);
+    public void NewChicken(Vector3 pos, Quaternion rot) { // new chicken's position and rotation
+        MyChicken(this.MyPrefab, this.MySound, pos, rot);
     }
 
     public void MakeNoise() {
